76d84f1a1933bf5654a1be720d3f780972e017e3
Junsuk Park <js@cre.ma>
2017-01-13 00:32:17 +0900

    Implemeted creation process for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,34 @@
 class PostsController < ApplicationController
-  def index
 
+  before_action :set_post, [:show, :edit, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: 'Your post was created successfully'
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:rationale, :date)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
