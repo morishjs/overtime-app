545c8619f9a40c8ddab0004fd47bd3108f1fae9a
Junsuk Park <js@cre.ma>
2017-02-05 16:38:40 +0900

    Implemented enum form status into posts for approval work flow

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ActiveRecord::Base
+  enum status: { submitted: 0, approved: 1, rejected: 2 }
   belongs_to :user
   validates_presence_of :rationale, :date
 end
+
+#p.submitted!
+#p.approved!
+#p.rejected!
